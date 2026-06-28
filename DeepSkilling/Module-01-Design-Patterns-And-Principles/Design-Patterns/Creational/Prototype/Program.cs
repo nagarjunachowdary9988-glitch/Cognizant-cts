0fbd657ef7dae90e66af32aefcffbce6a3b32c3e
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-28 13:18:11 +0530

    Completed Prototype pattern

---
+++ DeepSkilling/Module-01-Design-Patterns-And-Principles/Design-Patterns/Creational/Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿using Prototype;
+
+
+Document original =
+new Document(
+    "My Report",
+    "Design Patterns"
+);
+
+
+Document copy =
+(Document)original.Clone();
+
+
+Console.WriteLine("Original Document:");
+
+original.Display();
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("Cloned Document:");
+
+copy.Display();
