440f0354d5a5684530741b20818b0ec2f3504aaf
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-30 09:42:43 +0530

    Completed Delegates and Events in Week 2

---
+++ DeepSkilling/Week-2/Delegates-Events/Program.cs
@@ -0,0 +1,57 @@
+﻿// Delegate declaration
+
+delegate void MessageDelegate(string message);
+
+
+
+class Notification
+{
+
+    public void SendEmail(string message)
+    {
+        Console.WriteLine(
+            "Email: " + message
+        );
+    }
+
+
+
+    public void SendSMS(string message)
+    {
+        Console.WriteLine(
+            "SMS: " + message
+        );
+    }
+
+}
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+        Notification notification =
+            new Notification();
+
+
+
+        MessageDelegate notify;
+
+
+
+        notify = notification.SendEmail;
+
+        notify("Meeting at 10 AM");
+
+
+
+        notify += notification.SendSMS;
+
+        notify("Your OTP is 1234");
+
+    }
+
+}
