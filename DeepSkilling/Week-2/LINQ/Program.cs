b930635dbee521c2f3f9de62bd1e098184b561a2
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-30 09:40:23 +0530

    Completed LINQ in Week 2

---
+++ DeepSkilling/Week-2/LINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+
+List<int> numbers = new List<int>()
+{
+    10,20,30,40,50
+};
+
+
+// Filtering using LINQ
+
+var evenNumbers =
+    numbers.Where(n => n % 20 == 0);
+
+
+
+Console.WriteLine("Numbers divisible by 20:");
+
+foreach(var num in evenNumbers)
+{
+    Console.WriteLine(num);
+}
+
+
+
+// Sorting
+
+var sortedNumbers =
+    numbers.OrderByDescending(n => n);
+
+
+
+Console.WriteLine("Sorted numbers:");
+
+foreach(var num in sortedNumbers)
+{
+    Console.WriteLine(num);
+}
