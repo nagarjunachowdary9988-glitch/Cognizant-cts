0821c0fd3f54f330623fa64d5899416dc42bd4d8
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-30 09:50:05 +0530

    Completed Logging in Week 2

---
+++ DeepSkilling/Week-2/Logging/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+
+
+
+using var loggerFactory =
+    LoggerFactory.Create(builder =>
+    {
+        builder
+        .AddConsole();
+    });
+
+
+
+ILogger logger =
+    loggerFactory.CreateLogger<Program>();
+
+
+
+logger.LogInformation(
+    "Application Started"
+);
+
+
+
+try
+{
+
+    int a = 10;
+
+    int b = 0;
+
+
+    int result = a / b;
+
+
+    Console.WriteLine(result);
+
+}
+
+
+catch(Exception ex)
+{
+
+    logger.LogError(
+        ex,
+        "Error occurred while dividing numbers"
+    );
+
+}
+
+
+
+logger.LogInformation(
+    "Application Ended"
+);
