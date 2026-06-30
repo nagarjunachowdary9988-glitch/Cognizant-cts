7f11480d16a23f8c2332a9edbdfc05429cbb3654
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-30 09:38:30 +0530

    Completed File Handling in Week 2

---
+++ DeepSkilling/Week-2/File-Handling/Program.cs
@@ -0,0 +1,37 @@
+﻿string filePath = "sample.txt";
+
+
+// Write data into file
+
+File.WriteAllText(
+    filePath,
+    "Welcome to .NET File Handling"
+);
+
+
+Console.WriteLine("File created successfully");
+
+
+// Read data from file
+
+string data =
+    File.ReadAllText(filePath);
+
+
+Console.WriteLine(
+    "File Content: " + data
+);
+
+
+
+// Append data
+
+File.AppendAllText(
+    filePath,
+    "\nAdding new line"
+);
+
+
+Console.WriteLine(
+    "Data appended"
+);
