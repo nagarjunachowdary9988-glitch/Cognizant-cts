880c4efdf6183b39c10becd9c2eda99b0c7a899f
Nagarjuna Chowdary <nagarjunachowdary9988@gmail.com>
2026-06-30 09:52:42 +0530

    Completed NuGet packages in Week 2

---
+++ DeepSkilling/Week-2/NuGet/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+
+class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+}
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+        Student student = new Student()
+        {
+            Id = 1,
+            Name = "Nagarjuna"
+        };
+
+
+        // Object to JSON
+
+        string json =
+            JsonConvert.SerializeObject(student);
+
+
+        Console.WriteLine(json);
+
+
+
+        // JSON to Object
+
+        Student result =
+            JsonConvert.DeserializeObject<Student>(json);
+
+
+
+        Console.WriteLine(
+            result.Name
+        );
+
+    }
+
+}
